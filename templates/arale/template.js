3ed0b1bb5b93524b2b7ffe7bd9db91208b87cb0b
popomore <sakura9515@gmail.com>
2013-03-14 11:31:25 +0800

    update arale template

--- templates/arale/template.js
+++ templates/arale/template.js
@@ -1,68 +1,60 @@
-/*
- * grunt-init-jquery
- * https://gruntjs.com/
- *
- * Copyright (c) 2012 'Cowboy' Ben Alman, contributors
- * Licensed under the MIT license.
- */
+ 'use strict';
 
-'use strict';
+exports.description = 'Create Arale module.';
 
-// Basic template description.
-exports.description = 'Create a jQuery plugin, including QUnit unit tests.';
+exports.notes = '';
 
-// Template-specific notes to be displayed before question prompts.
-exports.notes = '111';
-
-// Template-specific notes to be displayed after question prompts.
-exports.after = '333';
-
-// Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
 
-// The actual init template.
 exports.template = function(grunt, init, done) {
+
+  grunt.util._.extend(init.prompts, {
+    family: {
+      message: 'your CMD family',
+      default: 'arale',
+      warning: ''
+    }
+  });
 
   init.process({type: 'arale'}, [
     // Prompt for these values.
     init.prompt('name'),
+    init.prompt('family'),
     init.prompt('version', '1.0.0'),
-    init.prompt('root'),
     init.prompt('description', 'The best jQuery plugin ever.'),
     init.prompt('repository'),
     init.prompt('homepage'),
     init.prompt('bugs'),
     init.prompt('licenses', 'MIT')
   ], function(err, props) {
-    // Files to copy (and process).
-    //var files = init.filesToCopy(props);
-//
-    //// Actually copy (and process) files.
-    ////init.copyAndProcess(files, props, {noProcess: 'libs/**'});
-//
-    //// Generate package.json file, used by npm and grunt.
-    //init.writePackageJSON('package.json', {
-        //'name': props.name,
-        //'version': '1.0.0',
-        //'root': props.root,
-        //'description': props.description || '',
-        //'homepage': props.description || '',
-        //'author': props.description || '',
-        //'repository': {
-            //'type': 'git',
-            //'url': ''
-        //},
-        //'bugs': {
-            //'url': ''
-        //},
-        //'license': 'MIT',
-        //'dependencies': {},
-        //'tests': [props.name],
-        //'output': {}
-    //});
+    var files = init.filesToCopy(props);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // Generate package.json file, used by npm and grunt.
+    init.writePackageJSON('package.json', {
+      'family': props.family,
+      'name': props.name,
+      'version': '1.0.0',
+      'root': props.family,
+      'description': props.description ,
+      'homepage': props.homepage,
+      'author': props.author,
+      'repository': '',
+      'bugs': '',
+      'licenses': ['MIT'],
+      'dependencies': {},
+      'tests': [props.name],
+      'output': {}
+    }, function(pkg, props) {
+      ['family', 'root', 'tests', 'output', 'author'].forEach(function(prop) {
+        if (prop in props) { pkg[prop] = props[prop]; }
+      });
+      return pkg;
+    });
 
     // All done!
     done();
   });
-
 };
