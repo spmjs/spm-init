26e0dbc3e05e3f1158f1b3954da2301fe055cc7f
Hsiaoming Yang <me@lepture.com>
2013-04-11 17:08:04 +0800

    remove version on scripts

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -10,7 +10,7 @@ try {
 } catch(e) {
   console.log('  you need install spm to register the program');
   console.log();
-  console.log('    \x1b[31m$ npm install spm@~2.0.0 -g\x1b[39m');
+  console.log('    \x1b[31m$ npm install spm -g\x1b[39m');
   console.log();
   console.log("  if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
   console.log();
